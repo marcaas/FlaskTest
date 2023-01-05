f59ea70d6586ca04a18698f4d33bf7ae45093bbd
marcaas <t13274515361@gmail.com>
2023-01-05 09:34:24 +0800

    2023/1/5

---
+++ demo03/app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+class User:
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
+
+
+@app.route('/')
+def hello_world():
+    user = User(username="marcaas", email="marcaas@china.com")
+    person = {
+        "username": "张三",
+        "email": "zhangsan@qq.com"
+    }
+    return render_template("index.html", user=user, person = person)
+
+
+@app.route('/blog/<blog_id>')
+def blog_detail(blog_id):
+    return render_template("blog_detail.html", blog_id=blog_id, username="marcaas")
+
+
+@app.route('/filter')
+def filter():
+    user = User(username="marcaas", email="marcaas@china.com")
+    return render_template("filter.html", user=user)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
