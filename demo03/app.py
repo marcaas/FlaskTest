a072a8f11f459368d4c3703bb055ff540c5fb007
marcaas <t13274515361@gmail.com>
2023-01-09 10:17:59 +0800

    2023/1/9

--- demo03/app.py
+++ demo03/app.py
@@ -12,25 +12,25 @@ class User:
 
 @app.route('/')
 def hello_world():
-    user = User(username="marcaas", email="marcaas@china.com")
+    user = User(username = "marcaas", email = "marcaas@china.com")
     person = {
         "username": "zhangsan",
         "email": "zhangsan@qq.com"
     }
-    return render_template("index.html", user=user, person = person)
+    return render_template("index.html", user = user, person = person)
 
 
 @app.route('/blog/<blog_id>')
 def blog_detail(blog_id):
-    return render_template("blog_detail.html", blog_id=blog_id, username="marcaas")
+    return render_template("blog_detail.html", blog_id = blog_id, username = "marcaas")
 
 
 @app.route('/filter')
 def filter():
-    user = User(username="marcaas", email="marcaas@china.com")
-    return render_template("filter.html", user=user)
+    user = User(username = "marcaas", email = "marcaas@china.com")
+    return render_template("filter.html", user = user)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug = True)
 
